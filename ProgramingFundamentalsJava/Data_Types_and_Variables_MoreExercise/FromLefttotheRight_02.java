4818e9a7493352890154948a00a85ed527853e4d
Svetlin <prolama6@gmail.com>
2022-05-28 12:35:59 +0300

    new task

---
+++ ProgramingFundamentalsJava/Data_Types_and_Variables_MoreExercise/FromLefttotheRight_02.java
@@ -0,0 +1,10 @@
+package ProgramingFundamentalsJava.Data_Types_and_Variables_MoreExercise;
+
+import java.util.Scanner;
+
+public class FromLefttotheRight_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
