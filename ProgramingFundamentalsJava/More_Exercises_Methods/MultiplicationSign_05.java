ae18cb5f73cb2bcdbfd9eec83567598762545461
Svetlin <prolama6@gmail.com>
2022-06-12 21:04:59 +0300

    new task

---
+++ ProgramingFundamentalsJava/More_Exercises_Methods/MultiplicationSign_05.java
@@ -0,0 +1,10 @@
+package ProgramingFundamentalsJava.More_Exercises_Methods;
+
+import java.util.Scanner;
+
+public class MultiplicationSign_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
