f4b30ba247df6ac11313b1075c95674be4a4cb31
Svetlin <prolama6@gmail.com>
2022-06-15 07:20:02 +0300

    new task

---
+++ ProgramingFundamentalsJava/Arrays_Exercise/EqualSums_Lists_06.java
@@ -0,0 +1,10 @@
+package ProgramingFundamentalsJava.Arrays_Exercise;
+
+import java.util.Scanner;
+
+public class EqualSums_Lists_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
