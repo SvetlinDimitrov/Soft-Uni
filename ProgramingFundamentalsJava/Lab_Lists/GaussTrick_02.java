a6c983eeecf73ca6c432e30ce4b9e008b71a53d5
Svetlin <prolama6@gmail.com>
2022-06-15 14:35:17 +0300

    new task

---
+++ ProgramingFundamentalsJava/Lab_Lists/GaussTrick_02.java
@@ -0,0 +1,10 @@
+package ProgramingFundamentalsJava.Lab_Lists;
+
+import java.util.Scanner;
+
+public class GaussTrick_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
