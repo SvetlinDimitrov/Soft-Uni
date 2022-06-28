45fd478c1a64631e81bf91c24680f0516680488a
Svetlin <prolama6@gmail.com>
2022-06-28 13:04:37 +0300

    new task

---
+++ AlgorithmsFundamentalsWithJava/Combinatorial_Problems_Lab/CombinationsWithoutRepetition_05.java
@@ -0,0 +1,24 @@
+package AlgorithmsFundamentalsWithJava.Combinatorial_Problems_Lab;
+
+import java.util.Scanner;
+
+public class CombinationsWithoutRepetition_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] arr = scanner.nextLine().split("\\s+");
+        int big = Integer.parseInt(scanner.nextLine());
+        String [] copy = new String [big];
+        Combination(arr , copy , 0 ,0 );
+    }
+
+    private static void Combination(String[] arr, String[] copy, int index, int start) {
+        if(index >= copy.length){
+            System.out.println(String.join(" ",copy));
+            return;
+        }
+        for (int i = start; i < arr.length ; i++) {
+            copy[index] = arr [i];
+            Combination(arr , copy , index +1 , start + 1 );
+        }
+    }
+}
