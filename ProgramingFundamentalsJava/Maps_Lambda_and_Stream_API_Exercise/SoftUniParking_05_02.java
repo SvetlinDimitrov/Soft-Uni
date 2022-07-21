35f18dace9bb7a54a20f1d3e49dff60370e424c4
Svetlin <prolama6@gmail.com>
2022-07-21 19:27:05 +0300

    new task

---
+++ ProgramingFundamentalsJava/Maps_Lambda_and_Stream_API_Exercise/SoftUniParking_05_02.java
@@ -0,0 +1,10 @@
+package ProgramingFundamentalsJava.Maps_Lambda_and_Stream_API_Exercise;
+
+import java.util.Scanner;
+
+public class SoftUniParking_05_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
